71068ae13cceec40da23c4c8d49ab4b3e4bae74f
alekrop <arctifoxslin@gmail.com>
2026-04-26 20:50:43 +0300

    test: fix jest ESM support for immer and redux toolkit

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
   },
   transformIgnorePatterns: [
-    "node_modules/(?!(react-redux|@react-native|react-native|@react-navigation)/)",
+    "node_modules/(?!(react-redux|@reduxjs|redux-persist|immer|@react-native|react-native|@react-navigation|@notifee|@react-native-async-storage)/)",
   ],
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
