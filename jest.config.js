e83a2ace833c35d8463de998ec97b47592b7e0b7
alekrop <arctifoxslin@gmail.com>
2026-04-26 21:02:23 +0300

    test: mock react-native to fix jest flow parsing error

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,9 @@ module.exports = {
   transformIgnorePatterns: [
     "node_modules/(?!(react-redux|@reduxjs|redux-persist|immer|@react-native|react-native|@react-navigation|@notifee|@react-native-async-storage)/)",
   ],
+  moduleNameMapper: {
+    "^react-native$": "<rootDir>/__mocks__/react-native.js",
+  },
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
