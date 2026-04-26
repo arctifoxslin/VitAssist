105f619c2cb5dd9755be76e0cab05bbc1300770d
alekrop <arctifoxslin@gmail.com>
2026-04-26 21:31:33 +0300

    test: update moduleNameMapper and create mock date-time-picker

---
+++ __mocks__/@react-native-community/datetimepicker.js
@@ -0,0 +1,3 @@
+module.exports = function DateTimePicker() {
+    return null;
+};
--- jest.config.js
+++ jest.config.js
@@ -12,6 +12,7 @@ module.exports = {
     "^@react-navigation/native$": "<rootDir>/__mocks__/@react-navigation/native.js",
     "^@react-navigation/native-stack$": "<rootDir>/__mocks__/@react-navigation/native-stack.js",
     "^react-native-vector-icons/(.*)$": "<rootDir>/__mocks__/react-native-vector-icons/$1.js",
+    "^@react-native-community/datetimepicker$": "<rootDir>/__mocks__/@react-native-community/datetimepicker.js",
   },
   setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
