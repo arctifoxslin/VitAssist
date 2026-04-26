f877fd8cfdcbe5c99413b80199f7f5a778686bdc
alekrop <arctifoxslin@gmail.com>
2026-04-26 21:35:23 +0300

    test: mock createNavigationContainerRef for jest

--- __mocks__/@react-navigation/native.js
+++ __mocks__/@react-navigation/native.js
@@ -2,4 +2,10 @@ module.exports = {
     NavigationContainer: ({ children }) => children,
     useNavigation: () => ({ navigate: () => { } }),
     useRoute: () => ({ params: {} }),
+    createNavigationContainerRef: () => ({
+        current: null,
+        navigate: () => { },
+        goBack: () => { },
+        reset: () => { },
+    }),
 };
