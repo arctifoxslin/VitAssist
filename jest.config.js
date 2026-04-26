7ae31ced4192efe67fb3aaf925c6cb30c3dd73b4
alekrop <arctifoxslin@gmail.com>
2026-04-26 20:20:38 +0300

    ci: fix unused imports and update hooks  dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-  preset: 'react-native',
+  preset: "react-native",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+  },
+  transformIgnorePatterns: [
+    "node_modules/(?!(react-redux|@react-native|react-native|@react-navigation)/)",
+  ],
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
